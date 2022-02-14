94e423e8e2e73c952fb9125529dfe90b7fa0d04e
Avvy <saberwatch@gmail.com>
2022-02-13 21:52:40 -0800

    touch up

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 import type { NextPage } from "next";
 import Image from "next/image";
 import Head from "../components/Head";
-import styles from "../styles/Home.module.css";
+import styles from "../styles/Home.module.scss";
 
 const Home: NextPage = () => {
   return (
