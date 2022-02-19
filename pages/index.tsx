2afc8b4ad208a09d0202c2a418c263da534aea49
Avvy <saberwatch@gmail.com>
2022-02-18 23:40:25 -0800

    full project

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,46 @@
 import type { NextPage } from "next";
 import Image from "next/image";
+import { useEffect, useState } from "react";
 import Head from "../components/Head";
 import styles from "../styles/Home.module.scss";
 
+//{"slip": { "id": 16, "advice": "It's unlucky to be superstitious."}}
+
+interface QuoteResponse {
+  slip: {
+    id: number;
+    advice: string;
+  };
+}
+
 const Home: NextPage = () => {
+  const [saying, setSaying] = useState<QuoteResponse>();
+
+  async function quote() {
+    const word = await fetch("https://api.adviceslip.com/advice");
+    const json: QuoteResponse = await word.json();
+    setSaying(json);
+  }
+
+  useEffect(() => {
+    quote();
+  }, []);
+
   return (
     <>
       <Head />
-      <div className={styles.container}></div>
+      <div className={styles.container}>
+        <div className={styles.content}>
+          <p className={styles.number}>Advice #{saying?.slip.id}</p>
+          <p className={styles.quote}>"{saying?.slip.advice}"</p>
+        </div>
+
+        <img src="/img/pattern-divider-mobile.svg" />
+
+        <button className={styles.button} onClick={() => quote()}>
+          <img src="/img/icon-dice.svg" />
+        </button>
+      </div>
     </>
   );
 };
